4fb74e23edb9aae9a86e34fe223c25849ef6d849
nicozhang <zhangchenkai@outlook.com>
2016-02-27 17:07:40 +0800

    initial commit

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python
+"""
+WSGI config for myproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
 import os
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/venv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    exec(compile(open(virtualenv, "rb").read(), virtualenv, 'exec'), dict(__file__=virtualenv))
-except IOError:
-    pass
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
-from myblog.wsgi import application
+from django.core.wsgi import get_wsgi_application
+
+# GETTING-STARTED: change 'myproject' to your project name:
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
+
+application = get_wsgi_application()
