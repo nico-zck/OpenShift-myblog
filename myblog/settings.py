24cb4dc9eb666c77b2ffcbbf8482c56a63befdb3
nicozhang <zhangchenkai@outlook.com>
2016-02-27 22:30:00 +0800

    initial commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi','static')
 STATICFILES_DIRS = (
     'templates/static',
 )
